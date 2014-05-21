78c48b0c0a09420322c03516780b8ec2501757bf
AliveDevil <alivedevil@fibre-network.de>
2014-05-21 18:13:49 +0200

    Removed *.generated.* from gitignore. Was a stupid decision to remove these files.

---
+++ libargument/Controller.generated.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using libargument.Conversion;
+
+namespace libargument
+{
+	public abstract partial class Controller
+	{
+		private void registerDefaultConverter()
+		{
+				RegisterTypeConverter<Int16>(new Int16Converter());
+				RegisterTypeConverter<Int32>(new Int32Converter());
+				RegisterTypeConverter<Int64>(new Int64Converter());
+				RegisterTypeConverter<UInt16>(new UInt16Converter());
+				RegisterTypeConverter<UInt32>(new UInt32Converter());
+				RegisterTypeConverter<UInt64>(new UInt64Converter());
+				RegisterTypeConverter<Single>(new SingleConverter());
+				RegisterTypeConverter<Double>(new DoubleConverter());
+				RegisterTypeConverter<DateTime>(new DateTimeConverter());
+				RegisterTypeConverter<TimeSpan>(new TimeSpanConverter());
+				RegisterTypeConverter<Boolean>(new BooleanConverter());
+
+		}
+	}
+}
