c3923b756e6b4fd23ebabaa6920b62af13e9866d
AliveDevil <alivedevil@fibre-network.de>
2014-05-22 14:28:50 +0200

    Screwed up version. Fixed it.

--- libargument/Properties/AssemblyInfo.cs
+++ libargument/Properties/AssemblyInfo.cs
@@ -14,4 +14,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("b29fc4ca-cf38-45c9-8ab3-4969a3f5cb17")]
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
