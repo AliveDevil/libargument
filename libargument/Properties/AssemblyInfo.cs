0ca0c081ecd2c2896199294455b1edb3d30eed01
AliveDevil <alivedevil@fibre-network.de>
2014-05-22 16:06:42 +0200

    Version 0.1.0.0 .NET 4.0 Client Profile. .NET 3.5 Client Profile. Removed target "Tests"

--- libargument/Properties/AssemblyInfo.cs
+++ libargument/Properties/AssemblyInfo.cs
@@ -13,4 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b29fc4ca-cf38-45c9-8ab3-4969a3f5cb17")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersionAttribute("0.1.0.0")]
