94b2a031a55e0a7aba7b4a17de5be73a36369e9a
AliveDevil <alivedevil@fibre-network.de>
2014-05-22 13:39:26 +0200

    Reformat Controller.tt and Controller.generated.cs

--- libargument/Controller.generated.cs
+++ libargument/Controller.generated.cs
@@ -7,18 +7,18 @@ namespace libargument
 	{
 		private void registerDefaultConverter(IController controller)
 		{
-				RegisterTypeConverter<Int16>(new Int16Converter(controller));
-				RegisterTypeConverter<Int32>(new Int32Converter(controller));
-				RegisterTypeConverter<Int64>(new Int64Converter(controller));
-				RegisterTypeConverter<UInt16>(new UInt16Converter(controller));
-				RegisterTypeConverter<UInt32>(new UInt32Converter(controller));
-				RegisterTypeConverter<UInt64>(new UInt64Converter(controller));
-				RegisterTypeConverter<Single>(new SingleConverter(controller));
-				RegisterTypeConverter<Double>(new DoubleConverter(controller));
-				RegisterTypeConverter<DateTime>(new DateTimeConverter(controller));
-				RegisterTypeConverter<TimeSpan>(new TimeSpanConverter(controller));
-				RegisterTypeConverter<Boolean>(new BooleanConverter(controller));
-				RegisterTypeConverter<String>(new StringConverter(controller));
+			RegisterTypeConverter<Int16>(new Int16Converter(controller));
+			RegisterTypeConverter<Int32>(new Int32Converter(controller));
+			RegisterTypeConverter<Int64>(new Int64Converter(controller));
+			RegisterTypeConverter<UInt16>(new UInt16Converter(controller));
+			RegisterTypeConverter<UInt32>(new UInt32Converter(controller));
+			RegisterTypeConverter<UInt64>(new UInt64Converter(controller));
+			RegisterTypeConverter<Single>(new SingleConverter(controller));
+			RegisterTypeConverter<Double>(new DoubleConverter(controller));
+			RegisterTypeConverter<DateTime>(new DateTimeConverter(controller));
+			RegisterTypeConverter<TimeSpan>(new TimeSpanConverter(controller));
+			RegisterTypeConverter<Boolean>(new BooleanConverter(controller));
+			RegisterTypeConverter<String>(new StringConverter(controller));
 
 		}
 	}
