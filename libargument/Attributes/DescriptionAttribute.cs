c73b7e5682bbb1e0674fc04edac9a307bb9cde59
AliveDevil <alivedevil@fibre-network.de>
2014-06-02 14:22:23 +0200

    Added DescriptionAttribute.

---
+++ libargument/Attributes/DescriptionAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace libargument.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+	public sealed class DescriptionAttribute : Attribute
+	{
+		private readonly string longDescription;
+
+		private readonly string shortDescription;
+
+		public DescriptionAttribute(string shortDescription, string longDescription)
+		{
+			this.shortDescription = shortDescription;
+			this.longDescription = longDescription;
+		}
+
+		public string LongDescription
+		{
+			get { return longDescription; }
+		}
+
+		public string ShortDescription
+		{
+			get { return shortDescription; }
+		}
+	}
+}
