e3b8e2dbe4801bf80a5ddb453b2eaba8dd389881
AliveDevil <alivedevil@fibre-network.de>
2014-06-02 13:16:38 +0200

    Added nuget directory to .gitignore. Updated versioning - automatically if TextTemplates are executed.

--- libargument/Properties/AssemblyInfo.cs
+++ libargument/Properties/AssemblyInfo.cs
@@ -4,14 +4,22 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("libargument")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple library for parsing command line arguments.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#elif NET35
+[assembly: AssemblyConfiguration("Release NET35")]
+#elif NET40
+[assembly: AssemblyConfiguration("Release NET40")]
+#else
 [assembly: AssemblyConfiguration("")]
+#endif
 [assembly: AssemblyCompany("AliveDevil")]
-[assembly: AssemblyProduct("libargument.Properties")]
+[assembly: AssemblyProduct("libargument")]
 [assembly: AssemblyCopyright("© 2014 AliveDevil")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.59")]
+[assembly: AssemblyFileVersion("0.1.0.59")]
+[assembly: AssemblyInformationalVersion("v0.1.0-pre")]
 [assembly: Guid("b29fc4ca-cf38-45c9-8ab3-4969a3f5cb17")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersionAttribute("0.1.0.0")]
