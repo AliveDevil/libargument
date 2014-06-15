cac7383684543e513a4cf6bb35333be08ba9ce30
AliveDevil <alivedevil@fibre-network.de>
2014-06-15 10:38:43 +0200

    Added Option-Attribute allowing options for multiple methods without declaring it thousands of times.

---
+++ libargument/Attributes/OptionAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace libargument.Attributes
+{
+	/// <summary>
+	/// Describes a field in a controller class as an option.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+	public sealed class OptionAttribute : Attribute
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public OptionAttribute()
+		{
+		}
+	}
+}
